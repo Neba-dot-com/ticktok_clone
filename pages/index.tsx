3d781adcbb65af0843a916024a9009faf03af06a
neba <neba@neba.neba>
2023-01-24 08:57:27 +0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
+import axios from 'axios';
+import { Video } from '../types';
+import VideoCard from '../components/VideoCard';
+import NoResults from '../components/NoResults';
+
+import { BASE_URL } from '../utils';
+
+// const inter = Inter({ subsets: ['latin'] })
+interface IProps {
+  videos:Video[]
+}
+
+
+
+export default function Home({videos}:IProps) {
+  
+  return (
+    <h1 className="flex flex-col gap-10 videos h-full">
+    {videos.length? (videos.map((video:Video)=>(
+      <VideoCard post={video} key={video._id}/>
+    ))
+    ):(<NoResults text={'No Videos'}/> )}
+
+    </h1>
+  )
+}
+ export const getServerSideProps=async({query:{topic}}:{query:{topic:string}})=>{
+
+let response = null;
+
+if(topic){
+  response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+}else{
+  response = await axios.get(`${BASE_URL}/api/post`);
+}
+
+ 
+  
+  return {
+    props:{
+      videos:response.data
+    }
+ }
+}
---
+++ utils/client.ts
@@ -0,0 +1,34 @@
+import sanityClient from '@sanity/client';
+
+export const client = sanityClient({
+  projectId: '17568dxx',
+  dataset: 'production',
+  apiVersion: '2022-03-10',
+  useCdn: false,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+});
+// sk1j6lg81wGopq3vdgNEyINViK28vgcDncGG5x44tvSK0jqCUtUSEPj3ZbebekaaRrvVAc1kkzKRIMH1zh22dRu1M4MguNUO9HeqcSlJQ7VUveNtkZtSgNux7aJVJe8YVzzFszXhj9d50jJLCbITrXFEHcPT4ygE5u2hdMiAtb03KuQh8f1y
+
+
+// "@next/font": "13.1.1",
+// "@react-oauth/google": "^0.5.1",
+// "@sanity/client": "^3.3.0",
+// "@types/node": "18.11.18",
+// "@types/react": "18.0.26",
+// "@types/react-dom": "18.0.10",
+// "axios": "^0.27.2",
+// "eslint": "8.30.0",
+// "eslint-config-next": "13.1.1",
+// "gapi-script": "^1.2.0",
+// "jwt-decode": "^3.1.2",
+// "next": "13.1.1",
+// "react": "18.2.0",
+// "react-dom": "18.2.0",
+// "react-google-login": "^5.2.2",
+// "react-icons": "^4.3.1",
+// "typescript": "4.9.4",
+// "uuidv4": "^6.2.13",
+// "zustand": "^4.0.0-rc.1"
+// "autoprefixer": "^10.4.13",
+// "postcss": "^8.4.20",
+// "tailwindcss": "^3.2.4"
